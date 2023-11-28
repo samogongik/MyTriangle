914711c727e3321760f64ab6101c163c1404e777
samogongik <kozub.sa@phystech.edu>
2023-11-28 23:19:28 +0300

    added input check

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 #include "OctoTree.h"
 
 
 int main() {
     int amount = 0;
     std::cin >> amount;
+    assert(std::cin.good());
+
     Point3D min(0, 0, 0);
     Point3D max(0, 0, 0);
     std::vector<Tringle3D> tringles;
@@ -15,6 +18,7 @@ int main() {
         for(int j = 0; j < 3; j++) {
             double x,y,z;
             std::cin >> x >> y >> z;
+            assert(std::cin.good());
 
             if (min.x > x) {min.x = x;}
             if (min.y > y) {min.y = y;}
@@ -37,7 +41,11 @@ int main() {
         octree.insert(tringles[i-1]);
     }
 
-//    octree.print(amount);
+    octree.print(amount);
     bool flag = intersection_checking_tringles(tringles[0], tringles[1]);
     std:: cout << flag;
 }
+
+
+
+
